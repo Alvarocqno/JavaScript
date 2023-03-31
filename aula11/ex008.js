dd3fe3e4b1f3742589454f430c71342bc1f38ae4
Alvaro Costa <128705202+Alvarocqno@users.noreply.github.com>
2023-03-31 14:59:43 -0300

    atualização curso JS

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+
+var vel = 60.5
+console.log(`A Velocidade do seu carro é de ${vel}Km/h.`)
+if (vel > 60) {
+    console.log('Voce ultrapassou a velocidade permita. Foi MULTADO!!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
