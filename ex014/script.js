155a11c2fa87e6bc215f684d0a98ec8e2780e69b
Alvaro Costa <128705202+Alvarocqno@users.noreply.github.com>
2023-04-03 18:03:20 -0300

    ex014

--- ex014/script.js
+++ ex014/script.js
@@ -1,15 +1,20 @@
-function carregar () {
+function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        //bom dia
-    } else if (hora >= 12 && hora < 18) {
-        //boa tarde
-    } else {
-        //Boa noite
+    //var hora = data.getHours()
+    var hora = 12 
+    msg.innerHTML = `Agora sao ${hora} horas.`
+    if (hora >= 0 && hora <= 12 ){
+        //Bom Dia!
+        img.src = 'modelo/manha.png'
+        document.body.style.background = '#fee378'
+      } else if (hora >= 12 && hora < 18){
+            img.src = 'modelo/tarde.png'
+            document.body.style.background = '#22dfdb' 
+        } else {
+            img.src = 'modelo/noite.png'
+            document.body.style.background = "#403a56"
+        }
     }
 
-}
