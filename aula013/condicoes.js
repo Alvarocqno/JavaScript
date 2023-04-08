ab8e5ccefd2897f75cd6a841d06921498d8b92db
Alvaro Costa <128705202+Alvarocqno@users.noreply.github.com>
2023-04-08 19:06:11 -0300

    aula013

---
+++ aula013/condicoes.js
@@ -0,0 +1,11 @@
+var c = 1
+
+do {
+    console.log(`${c}`)
+    c++
+} while (c <= 5)
+
+while (c <= 10) {
+    console.log(`${c}`)
+    c++
+}
---
+++ aula013/node_fad6560edeaae.tmp.js
@@ -0,0 +1,10 @@
+var c = 1
+
+do {
+    console.log(`${c}`)
+    c++
+} while (c <= 5)
+
+while (c <= 10) {
+    console.log(`${c}`)
+}
