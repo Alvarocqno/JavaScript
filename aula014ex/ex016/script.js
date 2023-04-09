6b1b9ef3b227e1cea33b808e6c89d827dfafc8b4
Alvaro Costa <128705202+Alvarocqno@users.noreply.github.com>
2023-04-09 18:51:17 -0300

    ex aula 14

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,3 +1,14 @@
-function analisar() {
-    
+function contar() {
+    let inicio = document.getElementById("iini")
+    let fim = document.getElementById("ifim")
+    let passo = document.getElementById('ipasso')
+    let res = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('Os dados precisam ser preenchdidos')
+    } else {
+
+    }
+
+
 }
