3e2fbd7362b7537556edf0a34f7d12871f4a8f82
Alvaro Costa <128705202+Alvarocqno@users.noreply.github.com>
2023-04-10 20:15:08 -0300

    ex 016

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -7,8 +7,27 @@ function contar() {
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('Os dados precisam ser preenchdidos')
     } else {
+        res.innerHTML = 'Contando...<br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('PASSO INVALIDO! Considerando passo 1')
+            p = 1
+        }
+        if (i < f){
+            for (let c = i;c <= f; c+=p) {
+                res.innerHTML += `${c}`
+            }
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c}`
+                }
 
+            }
+        
+        }
+    
     }
 
 
-}
