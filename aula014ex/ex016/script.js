56762891f5e822ccc68ff6c423a04292adcfb9e6
Alvaro Costa <128705202+Alvarocqno@users.noreply.github.com>
2023-04-12 10:34:02 -0300

    conclusao ex016

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -6,28 +6,31 @@ function contar() {
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('Os dados precisam ser preenchdidos')
+        res.innerHTML = 'Impossivel contar'
     } else {
-        res.innerHTML = 'Contando...<br>'
+        res.innerHTML = 'Contando: <br>'
         let i = Number(inicio.value)
         let f = Number(fim.value)
-        let p = Number(passo.value)
+        let p = Number(passo.value) 
         if (p <= 0) {
             window.alert('PASSO INVALIDO! Considerando passo 1')
             p = 1
         }
         if (i < f){
+            //contagem crescente
             for (let c = i;c <= f; c+=p) {
-                res.innerHTML += `${c}`
+                res.innerHTML += `${c} \u{1F449}`
             }
         } else {
+            //contagem regressiva
             for(let c = i; c >= f; c -= p) {
-                res.innerHTML += `${c}`
-                }
-
+                res.innerHTML += `${c} \u{1F449}`
             }
-        
+           
         }
-    
+        res.innerHTML += `\u{1F3C1}`
     }
+    
+}
 
 
