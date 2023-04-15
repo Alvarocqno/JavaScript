82182f42b168e12389b32e18a7be1a8071af737d
Alvaro Costa <128705202+Alvarocqno@users.noreply.github.com>
2023-04-14 21:46:29 -0300

    aula016

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,4 +1,19 @@
-let nome = [bia, carlos, ana]
+let nome = ['ana', 'ama', 'bia']
 
-console.log(nome)
 
+nome.push('alfredo') //adiciona o item ao final do array, igual o insert do python
+let total = nome.length //pega quantos itens exitem no array
+let ord = nome.sort()  //ordena o array, igual no python
+console.log(total)
+console.log(ord)
+
+for(let pos = 0;pos < nome.length;pos++) {
+    console.log(`${nome[pos]}`)
+}
+
+for(let pos in nome) {          //metodo simplificado de usar o for
+    console.log(`${nome[pos]}`)
+}
+
+let pos = nome.indexOf('ana') //busca o valor dentro da array e retorna qual a chave que se encontra.
+console.log(`O valor ${'ana'} está na posição ${pos}`)
